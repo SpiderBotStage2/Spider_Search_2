63a4f0184043240b35ad3eab615090486763efdd
luis <luis@luis-Satellite-L505>
2015-04-25 20:22:45 -0600

    Aqui se hace la ventana. Solo cuenta con imports.

---
+++ src/spidersearchengine2/GUI/Ventana.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spidersearchengine2.GUI;
+import java.awt.Image;
+import javax.swing.*;
+import java.awt.Toolkit;
+
+/**
+ *
+ * @author luis
+ */
+public class Ventana {
+    
+}
