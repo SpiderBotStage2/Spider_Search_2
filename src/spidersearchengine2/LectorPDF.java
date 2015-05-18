1f6ca4a1e763209a68880ec1a53864e021cebca3
tavo2360 <tavo.2360@yahoo.com>
2015-05-18 16:55:58 -0600

    agregue variable para poder accesar desde otra clase y tambien el parrafo lo devuelve sin monosilabos y sin mayusculas y sin caracteres especiales

--- src/spidersearchengine2/LectorPDF.java
+++ src/spidersearchengine2/LectorPDF.java
@@ -4,18 +4,24 @@ import java.io.File;
 import org.apache.tika.Tika;
 
 public class LectorPDF {
+    public String ContenidoDelArchivo = null;
     File archivoParaLeer;
 //public static void main(String args[]) throws Exception {
     public LectorPDF(String p_RutaDelDocumento){
-
+        
         try {
           archivoParaLeer=new File(p_RutaDelDocumento);
           String contenidoDelArchivo=new Tika().parseToString(archivoParaLeer);
-          System.out.println(contenidoDelArchivo);
+          ContenidoDelArchivo=contenidoDelArchivo;
+          ContenidoDelArchivo = ContenidoDelArchivo.replaceAll("\\b\\w{1,3}\\b\\s?", ""); //elimina palabras de 3 letras o menos
+          ContenidoDelArchivo = ContenidoDelArchivo.replaceAll("[^a-zA-Z]", " "); // cambia todos los caracteres que no son letras por un espacio
+          ContenidoDelArchivo = ContenidoDelArchivo.replaceAll("\\s+", " "); //cambia todos los campos en blanco por por un solo espacio
+    
         }
         catch (Exception e) {
           e.printStackTrace();
         }
+        //return ContenidoDelArchivo;
 
     } 
 }
