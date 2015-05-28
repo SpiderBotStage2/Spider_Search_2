36a15e4efffa03e2b677eb1987434b72ffee6fa7
tavo2360 <tavo.2360@yahoo.com>
2015-05-28 02:48:21 -0600

    ejemplos de inicializacion de hils

--- src/Arboles/Hilos.java
+++ src/Arboles/Hilos.java
@@ -8,6 +8,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import logica.CreaciondeHeapPorTamanoDeDocumentos;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -21,19 +25,26 @@ import java.util.Scanner;
  */
 public class Hilos {
 
-public static void main(String[] args) {
-   Threads.DireccionArchivoAProcesar = 
-           "D:\\Users\\Gustavo\\Desktop\\2014098445-CE1103-20​15-01-R9.pdf";
-   Thread test1 = new Thread(new Threads());
-test1.start();
+public static void main(String[] args) throws InterruptedException {
+ 
+    
+    Threadsqueprocesan test1 = new Threadsqueprocesan (
+           "D:\\Users\\Gustavo\\Desktop\\2014098445-CE1103-20​15-01-R9.pdf");
+  // Thread test1 = new Thread(new Threads());
+//test1.run();
     //Threads.DireccionArchivoAProcesar = 
-     //      "D:\\Users\\Gustavo\\Desktop\\2014098445-CE1103-20​15-01-R10.pdf";    
-    //Thread test2 = new Thread(new Threads());
-//test2.start();        
-    }
+          //"D:\\Users\\Gustavo\\Desktop\\2014098445-CE1103-20​15-01-R10.pdf";    
+  
+
+Threadsqueprocesan test2 = new Threadsqueprocesan (
+           "D:\\Users\\Gustavo\\Desktop\\2014098445-CE1103-20​15-01-R10.pdf");
+//Thread test2 = new Thread(new Threads());
+
+test2.run();
+
     
     
-    /*KeywordsyRepeticiones.print();
+    //KeywordsyRepeticiones.print();
     String[] Arreglo = { 
         "D:\\Users\\Gustavo\\Desktop\\r4.docx",
         "D:\\Users\\Gustavo\\Desktop\\r5.docx",
@@ -52,6 +63,7 @@ test1.start();
         nuevo.insert("2");
         nuevo.print();*/
     }
+
    
    
 
